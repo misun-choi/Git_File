7943c156f7965325cf7f4228dce426c824d8ba45
Choi misun <somoj600@naver.com>
2023-08-15 21:21:33 +0900

    화면만 구성

---
+++ Wpf_ProductionManagement/Wpf_ProductionManagement/ComModules.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf_ProductionManagement
+{
+    public class ComModules
+    {
+        #region 숫자를 문자로 받아 셋째 자리마다 숫자를 찍어 문자로 리턴 - ToComma(string szData)
+
+        /// <summary>
+        /// 숫자를 문자로 받아 셋째 자리마다 숫자를 찍어 문자로 리턴
+        /// </summary>
+        /// <param name="szData">숫자</param>
+        public string ToComma(string szData)
+        {
+            string szReturn = string.Empty;
+            if (szData != "")
+            {
+                string szValue = szData.Replace(",", "");
+                Double data = Double.Parse(szValue);
+                szReturn = string.Format("{0:###,###,###,###,###,###}", data);
+
+            }
+            return szReturn;
+        }
+        #endregion
+
+       
+    }
+}
---
+++ Wpf_ProductionManagement/Wpf_ProductionManagement/DataManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace Wpf_ProductionManagement
+{
+    public class DataManager
+    {
+        public static OracleConnection m_oracleConn;
+        ComModules cm = new ComModules();
+        private BindingList<object> itemList = new BindingList<object>();
+        private BindingList<object> centerList = new BindingList<object>();
+
+        public static bool initDB()
+        {
+            string strConn = "Data Source=DBSVR19C;User Id=mschoi;Password=mschoi;";
+            m_oracleConn = new OracleConnection(strConn);
+            try
+            {
+                m_oracleConn.Open();
+            }
+            catch (OracleException e)
+            {
+                System.Windows.MessageBox.Show(e.Message + "\n프로그램을 종료합니다.", "Error");
+                return false;
+            }
+            return true;
+        }
+    }
+}
